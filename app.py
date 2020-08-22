825fb583b7e51a2d4cb2aaf226e137948f1bba74
James <jamesseventyseven@gmail.com>
2020-08-22 22:04:37 +0100

    Added Likes

--- app.py
+++ app.py
@@ -210,6 +210,39 @@ def stop_following(follow_id):
     return redirect(f"/users/{g.user.id}/following")
 
 
+@app.route('/users/<int:user_id>/likes', methods=['GET'])
+def show_liked_messages(user_id):
+    if not g.user:
+        flash("Access Unauthorized.", 'danger')
+        return redirect('/')
+    user = User.query.get_or_404(user_id)
+    return render_template('users/likes.html', user=user, likes=user.likes)
+
+
+@app.route('/messages/<int:message_id/like', methods=['POST'])
+def add_like(message_id):
+    """Will add and remove a message from a user's likes"""
+
+    if not g.user:
+        flash("Access Unauthorized.", 'danger')
+        return redirect('/')
+
+    liked_message = Message.query.get_or_404(message_id)
+    if liked_message.user_id == g.user.id:
+        return abort(403)
+
+    user_likes = g.user.likes
+
+    if liked_message in user_likes:
+        g.user.likes = [like for like in user_likes if like != liked_message]
+    else:
+        g.user.likes.append(liked_message)
+
+    db.session.commit()
+
+    return redirect('/')
+
+
 @app.route('/users/profile', methods=["GET", "POST"])
 def profile():
     """Update profile for current user."""
